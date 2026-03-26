fdb60593c102b67d01b713dd81f9cc9c86834324
Полина Опевалова <poopev@MacBook-Air-Polina.local>
2026-03-26 13:33:08 +0300

    fix: correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/taskorbit/',
+  base: '/TaskOrbit/',
   server: {
     port: 3000,
     proxy: {
